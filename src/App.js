5a94cb2134729f28ffa6212230e5bc76560751aa
Afolabi Akorede <brainconnect0@gmail.com>
2022-07-05 15:52:24 +0100

    create a component folder and add a calculator file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-function App() {
-	return (
-		null
-	);
+import React from 'react';
+import Calculator from './components/calculator';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.component {
+  render() {
+    return (
+      <Calculator />
+    );
+  }
 }
 
 export default App;
--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,1 +1,11 @@
 import React from 'react';
+// eslint-disable-next-line react/prefer-stateless-function
+class Calculator extends React.Component {
+  render() {
+    return (
+      <div className="head">I am Afolabi Akorede</div>
+    );
+  }
+}
+
+export default Calculator;
