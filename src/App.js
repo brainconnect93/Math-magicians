d5a572dc1d59ad277c3355944694b493f9a680f3
Afolabi Akorede <brainconnect0@gmail.com>
2022-07-06 09:44:07 +0100

    Add html code in the calculator.js file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Calculator from './components/calculator';
 
 // eslint-disable-next-line react/prefer-stateless-function
-class App extends React.component {
+class App extends React.Component {
   render() {
     return (
       <Calculator />
--- src/components/calculator.js
+++ src/components/calculator.js
@@ -3,7 +3,34 @@ import React from 'react';
 class Calculator extends React.Component {
   render() {
     return (
-      <div className="head">I am Afolabi Akorede</div>
+      <>
+        <section className="container">
+          <div className="calc-box">
+            <div className="screen"><span className="output">0</span></div>
+            <div className="buttons">
+              <button type="button" className="calc">AC</button>
+              <button type="button" className="calc">+/-</button>
+              <button type="button" className="calc">%</button>
+              <button type="button" className="calc active"><span>&#247;</span></button>
+              <button type="button" className="calc">7</button>
+              <button type="button" className="calc">8</button>
+              <button type="button" className="calc">9</button>
+              <button type="button" className="calc active">x</button>
+              <button type="button" className="calc">4</button>
+              <button type="button" className="calc">5</button>
+              <button type="button" className="calc">6</button>
+              <button type="button" className="calc active">-</button>
+              <button type="button" className="calc">1</button>
+              <button type="button" className="calc">2</button>
+              <button type="button" className="calc">3</button>
+              <button type="button" className="calc active">+</button>
+              <button type="button" className="zero">0</button>
+              <button type="button" className="calc">.</button>
+              <button type="button" className="calc active">=</button>
+            </div>
+          </div>
+        </section>
+      </>
     );
   }
 }
