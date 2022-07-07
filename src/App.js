6a38e243acd41c927c868602266bb540ee0f47be
Afolabi Akorede <brainconnect0@gmail.com>
2022-07-07 13:30:58 +0100

    Change App.js class component to function as requested by the code reviewer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import React from 'react';
 import Calculator from './components/calculator';
 
 // eslint-disable-next-line react/prefer-stateless-function
-class App extends React.Component {
-  render() {
-    return (
-      <Calculator />
-    );
-  }
+function App() {
+  return (
+    <Calculator />
+  );
 }
 
 export default App;
