8a2dd1751b407458a1505dff4b79082218eb1b84
Afolabi Akorede <brainconnect0@gmail.com>
2022-07-07 08:44:36 +0100

    Install big.js and add logic folder

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,32 +1,44 @@
 import React from 'react';
+import calculate from "../logic/calculate";
 // eslint-disable-next-line react/prefer-stateless-function
 class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+        this.handleClick = this.handleClick.bind(this);
+      }
+    
+      handleClick(event) {
+        this.setState((state) => calculate(state, event.target.textContent));
+      }
+    
   render() {
+    const { next, total } = this.state;
     return (
       <>
         <section className="container">
           <div className="calc-box">
-            <div className="screen"><span className="output">0</span></div>
+            <div className="screen"><span className="output">{next || total || 0}</span></div>
             <div className="buttons">
-              <button type="button" className="calc">AC</button>
-              <button type="button" className="calc">+/-</button>
-              <button type="button" className="calc">%</button>
-              <button type="button" className="calc active"><span>&#247;</span></button>
-              <button type="button" className="calc">7</button>
-              <button type="button" className="calc">8</button>
-              <button type="button" className="calc">9</button>
-              <button type="button" className="calc active">x</button>
-              <button type="button" className="calc">4</button>
-              <button type="button" className="calc">5</button>
-              <button type="button" className="calc">6</button>
-              <button type="button" className="calc active">-</button>
-              <button type="button" className="calc">1</button>
-              <button type="button" className="calc">2</button>
-              <button type="button" className="calc">3</button>
-              <button type="button" className="calc active">+</button>
-              <button type="button" className="zero">0</button>
-              <button type="button" className="calc">.</button>
-              <button type="button" className="calc active">=</button>
+              <button type="button" className="calc" onClick={this.handleClick}>AC</button>
+              <button type="button" className="calc" onClick={this.handleClick}>+/-</button>
+              <button type="button" className="calc" onClick={this.handleClick}>%</button>
+              <button type="button" className="calc active" onClick={this.handleClick}><span>&#247;</span></button>
+              <button type="button" className="calc" onClick={this.handleClick}>7</button>
+              <button type="button" className="calc" onClick={this.handleClick}>8</button>
+              <button type="button" className="calc" onClick={this.handleClick}>9</button>
+              <button type="button" className="calc active" onClick={this.handleClick}>x</button>
+              <button type="button" className="calc" onClick={this.handleClick}>4</button>
+              <button type="button" className="calc" onClick={this.handleClick}>5</button>
+              <button type="button" className="calc" onClick={this.handleClick}>6</button>
+              <button type="button" className="calc active" onClick={this.handleClick}>-</button>
+              <button type="button" className="calc" onClick={this.handleClick}>1</button>
+              <button type="button" className="calc" onClick={this.handleClick}>2</button>
+              <button type="button" className="calc" onClick={this.handleClick}>3</button>
+              <button type="button" className="calc active" onClick={this.handleClick}>+</button>
+              <button type="button" className="zero" onClick={this.handleClick}>0</button>
+              <button type="button" className="calc" onClick={this.handleClick}>.</button>
+              <button type="button" className="calc active" onClick={this.handleClick}>=</button>
             </div>
           </div>
         </section>
