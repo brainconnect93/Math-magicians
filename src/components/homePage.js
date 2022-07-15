cc8a3785edca189aa1ae8f926c1650da86af14fd
Afolabi Akorede <brainconnect0@gmail.com>
2022-07-15 07:26:40 +0100

    Style the math-magicians to suite the React router

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -10,10 +10,6 @@ const Home = () => (
       when an unknown printer took a galley of type and scrambled it to make a type.
       It has survived not only five centuries, but also the leap into electronic ,
       remaining essentially unchanged.Lorem Ipsum is simply dummy text of the printing.
-      Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s,
-      when an unknown printer took a galley of type and scrambled it to make a type spe.
-      It has survived not only five centuries, but also the leap into electronic typesetting,
-      remaining essentially unchanged.
     </p>
     <ul className="listContainer">
       <li className="listItem">Make simple calculations</li>
