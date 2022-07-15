c50344915fa268492cb00f1848ac3820e3aa75d0
Afolabi Akorede <brainconnect0@gmail.com>
2022-07-15 03:16:13 +0100

    Setup the React-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import "./App.css";
 import React from 'react';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
 import Calculator from './components/calculator';
 
 // eslint-disable-next-line react/prefer-stateless-function
 const App = () => (
-  <Calculator />
+  <>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/calculator" element={<Calculator />} />
+      <Route path="/quote" element={<Quote />} />
+    </Routes>
+  </>
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter } from "react-router-dom";
 import App from './App';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
